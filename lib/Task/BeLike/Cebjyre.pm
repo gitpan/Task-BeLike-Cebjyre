cb0b09f1bac3492fc4f168c2dfb8df6330ff8758
Glenn Fowler <cebjyre@cpan.org>
2010-11-05 04:28:19 +0000

    Import of CEBJYRE/Task-BeLike-Cebjyre-0.0.1 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-Cebjyre
    gitpan-cpan-version:      0.0.1
    gitpan-cpan-path:         CEBJYRE/Task-BeLike-Cebjyre-0.0.1.tar.gz
    gitpan-cpan-author:       CEBJYRE
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/Cebjyre.pm
@@ -0,0 +1,97 @@
+use strict;
+use warnings;
+package Task::BeLike::Cebjyre;
+BEGIN {
+  $Task::BeLike::Cebjyre::VERSION = '0.0.1';
+}
+
+# ABSTRACT: Modules I find handy from time to time...
+
+
+1;
+
+
+
+=pod
+
+=head1 NAME
+
+Task::BeLike::Cebjyre - Modules I find handy from time to time...
+
+=head1 VERSION
+
+version 0.0.1
+
+=head1 TASK CONTENTS
+
+=head2 Email stuff
+
+=head3 L<Email::Sender>
+
+=head3 L<MIME::Parser>
+
+Also gives us Moose
+
+=head2 Data Access
+
+=head3 L<Cache::Memcached::Fast>
+
+=head3 L<Net::LDAP>
+
+=head2 The Interwebs
+
+=head3 L<CGI::Session>
+
+=head2 Misc
+
+=head3 L<File::chdir>
+
+=head3 L<Log::Log4perl>
+
+=head3 L<Module::Find>
+
+=head3 L<Telephony::CountryDialingCodes>
+
+=head3 L<Template>
+
+=head3 L<Text::Context>
+
+=head3 L<Tie::SecureHash>
+
+=head3 L<XML::Quote>
+
+=head1 NAME
+
+Task::BeLike::Cebjyre - Modules I find handy from time to time...
+
+=head1 VERSION
+
+version 0.0.1
+
+=head1 AUTHOR
+
+Glenn Fowler <cebjyre@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Glenn Fowler.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 AUTHOR
+
+Glenn Fowler <cebjyre@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Glenn Fowler.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
+
