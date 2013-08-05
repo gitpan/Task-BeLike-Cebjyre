671ca156328676babbece6789654bf9d4547b10e
Glenn Fowler <cebjyre@cpan.org>
2013-08-05 02:49:46 +0000

    Import of CEBJYRE/Task-BeLike-Cebjyre-1.0.1 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-Cebjyre
    gitpan-cpan-version:      1.0.1
    gitpan-cpan-path:         CEBJYRE/Task-BeLike-Cebjyre-1.0.1.tar.gz
    gitpan-cpan-author:       CEBJYRE
    gitpan-cpan-maturity:     released

--- lib/Task/BeLike/Cebjyre.pm
+++ lib/Task/BeLike/Cebjyre.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Task::BeLike::Cebjyre;
 {
-  $Task::BeLike::Cebjyre::VERSION = '1.0.0';
+  $Task::BeLike::Cebjyre::VERSION = '1.0.1';
 }
 
 # ABSTRACT: Modules I find handy from time to time...
@@ -11,6 +11,7 @@ package Task::BeLike::Cebjyre;
 1;
 
 __END__
+
 =pod
 
 =head1 NAME
@@ -19,7 +20,7 @@ Task::BeLike::Cebjyre - Modules I find handy from time to time...
 
 =head1 VERSION
 
-version 1.0.0
+version 1.0.1
 
 =head1 TASK CONTENTS
 
@@ -108,7 +109,7 @@ listed as a prereq yet; should be fixed in MongoDB 0.43.
 
 =head2 My modules
 
-=head3 L<Dist::Zilla::PluginBundle::CEBJYRE>
+=head3 L<Dist::Zilla::PluginBundle::CEBJYRE> 0.4.0
 
 =head3 L<Net::Google::FederatedLogin>
 
@@ -126,10 +127,9 @@ Glenn Fowler <cebjyre@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Glenn Fowler.
+This software is copyright (c) 2013 by Glenn Fowler.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
